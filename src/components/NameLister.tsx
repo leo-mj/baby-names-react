b4c043ea72f9fa50ae71004ede5b12a78a357c7e
Leo <leo.maedje@gmail.com>
2022-07-22 14:36:45 +0200

    Added search bar

--- src/components/NameLister.tsx
+++ src/components/NameLister.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import babyData from "/home/2206-001-lm/Developer/academy/training/html-react/baby-names-react/src/data.json";
 import { BabyData } from "../utils/baby-interface";
 import { sortAlph } from "../utils/sortAlph";
+import { textInputFilter } from "../utils/text-input-filter";
 import { NamePresenter } from "./NamePresenter";
 import styles from "./name-style.module.css";
 
@@ -8,9 +10,23 @@ const nameData = babyData;
 const sortedNameData: BabyData[] = sortAlph(nameData);
 
 export function NameLister(): JSX.Element {
+  const [text, setText] = useState("");
+  const searchedNameData: BabyData[] = sortedNameData.filter((baby) =>
+    textInputFilter(baby, text)
+  );
+  console.log(text);
+  console.log(searchedNameData);
   return (
     <>
-      <p className={styles.body}>{sortedNameData.map(NamePresenter)} </p>
+      <input
+        className={styles.search}
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+      />
+      <section className={styles.body}>
+        {searchedNameData.map(NamePresenter)}{" "}
+      </section>
     </>
   );
 }
