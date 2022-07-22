381a73ce440a8db040965b19557717b004bbd421
Leo <leo.maedje@gmail.com>
2022-07-22 15:28:52 +0200

    Created filter

--- src/components/NameLister.tsx
+++ src/components/NameLister.tsx
@@ -3,6 +3,7 @@ import babyData from "/home/2206-001-lm/Developer/academy/training/html-react/ba
 import { BabyData } from "../utils/baby-interface";
 import { sortAlph } from "../utils/sortAlph";
 import { textInputFilter } from "../utils/text-input-filter";
+import { filterBySex } from "../utils/filter-by-sex";
 import { NamePresenter } from "./NamePresenter";
 import styles from "./name-style.module.css";
 
@@ -14,8 +15,21 @@ export function NameLister(): JSX.Element {
   const searchedNameData: BabyData[] = sortedNameData.filter((baby) =>
     textInputFilter(baby, text)
   );
-  console.log(text);
-  console.log(searchedNameData);
+  const [previousSex, setSex] = useState("");
+  const handleBoyFilter = () => {
+    setSex((previousSex) => (previousSex = "m"));
+  };
+  const handleGirlFilter = () => {
+    setSex((previousSex) => (previousSex = "f"));
+  };
+  const handleFilterOff = () => {
+    setSex((previousSex) => (previousSex = ""));
+  };
+
+  const filteredNameData: BabyData[] = searchedNameData.filter((baby) =>
+    filterBySex(baby, previousSex)
+  );
+
   return (
     <>
       <input
@@ -24,9 +38,24 @@ export function NameLister(): JSX.Element {
           setText(event.target.value);
         }}
       />
-      <section className={styles.body}>
-        {searchedNameData.map(NamePresenter)}{" "}
-      </section>
+      <button className={styles.filterbtn} onClick={handleFilterOff}>
+        off
+      </button>
+      <button className={styles.filterbtn} onClick={handleGirlFilter}>
+        f
+      </button>
+      <button className={styles.filterbtn} onClick={handleBoyFilter}>
+        m
+      </button>
+      <p>
+        {" "}
+        active filter:{" "}
+        <button className={styles.filterbtn}>
+          {" "}
+          {previousSex || "off"}{" "}
+        </button>{" "}
+      </p>
+      <p className={styles.body}>{filteredNameData.map(NamePresenter)} </p>
     </>
   );
 }
